fa9e8cd6e0aa13db1a13380695559f943f1979df
crow <59679082+cr-0w@users.noreply.github.com>
2023-03-20 16:42:16 -0400

    Update xordecrypter.c

--- xordecrypter.c
+++ xordecrypter.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /* place encoded shellcode here */
-unsigned char crowpuke[] = ""; 
+unsigned char crowpuke[] = "";
 
 int main(void) {
-	
 	char key = 'CROW'; 
+	printf("(+) decoded %d-bytes\n\n\r", sizeof(crowpuke)); 
+    printf("unsigned char crowpuke[] = \"");
 	for (int x = 0; x < sizeof(crowpuke) - 1; x++) {
 		crowpuke[x] = crowpuke[x] ^ key;
         printf("\\x%02x", crowpuke[x]);
 	}
-	
+    printf("\";");
     return 0;
-
 }
