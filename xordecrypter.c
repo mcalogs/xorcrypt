30a642de061505f1bf3b54b90f92b26b68b7f41e
crow <59679082+cr-0w@users.noreply.github.com>
2023-03-20 15:55:19 -0400

    Create xordecrypter.c

---
+++ xordecrypter.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+unsigned char crowpuke[] = "encodedshellcodehere";
+
+int main(void) {
+char key = 'CROW'; 
+	printf("(+) decoded xor-encrypted shellcode\n");
+	for (int x = 0; x < sizeof(crowpuke) - 1; x++) {
+		crowpuke[x] = crowpuke[x] ^ key;
+	}
+}
