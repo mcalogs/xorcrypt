a9500f0c333c670257e38fdf582dcf84e0b363fa
crow <59679082+cr-0w@users.noreply.github.com>
2023-03-20 15:56:21 -0400

    Update xordecrypter.c

--- xordecrypter.c
+++ xordecrypter.c
@@ -3,7 +3,7 @@
 unsigned char crowpuke[] = "encodedshellcodehere";
 
 int main(void) {
-char key = 'CROW'; 
+	char key = 'CROW'; 
 	printf("(+) decoded xor-encrypted shellcode\n");
 	for (int x = 0; x < sizeof(crowpuke) - 1; x++) {
 		crowpuke[x] = crowpuke[x] ^ key;
