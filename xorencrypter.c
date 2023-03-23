04250a2c25afc8af95799451879666e1588a60bd
crow <59679082+cr-0w@users.noreply.github.com>
2023-03-23 01:40:42 -0400

    Update xorencrypter.c

--- xorencrypter.c
+++ xorencrypter.c
@@ -5,25 +5,25 @@
 unsigned char crowPuke[] = "blackbird";
 
 int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        printf("(-) usage: xorencrypter.exe <key>\n");
-        return 1;
-    }
 
-    /* get key from user */
-    const char* key = argv[1];
-    const size_t keyLen = strlen(key);
-    size_t shellcodeLen = strlen((const char*)crowPuke);
+        if (argc < 2) {
+            printf("(-) usage: xorencrypter.exe <key>\n");
+            return 1;
+        }
 
-    printf("(*) encoding with key: %s\n", key);
-    printf("(+) encoded %zd-bytes\n\n\r", shellcodeLen);
-    printf("unsigned char crowPuke[] = \"");
-    for (int i = 0; i < shellcodeLen; ++i) {
-        crowPuke[i] ^= key[i % keyLen];
-        printf("\\x%02x", crowPuke[i]);
-    }
-    
-    printf("\";\n");
-    return 0;
-    
+        const char* key = argv[1]; /* get key from user */
+        const size_t keyLen = strlen(key);
+        size_t shellcodeLen = strlen((const char*)crowPuke);
+
+        printf("(*) encoding with key: %s\n", key);
+        printf("(+) encoded %zd-bytes\n\n\r", shellcodeLen);
+        printf("unsigned char crowPuke[] = \"");
+        for (int i = 0; i < shellcodeLen; ++i) {
+            crowPuke[i] ^= key[i % keyLen];
+            printf("\\x%02x", crowPuke[i]);
+        }
+
+        printf("\";\n");
+        return 0;
+
 }
