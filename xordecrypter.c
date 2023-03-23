2d462353c92ac254d4664d30409868aaa16b9d9f
crow <59679082+cr-0w@users.noreply.github.com>
2023-03-23 00:40:59 -0400

    Update xordecrypter.c

--- xordecrypter.c
+++ xordecrypter.c
@@ -3,18 +3,24 @@
 /* place encoded shellcode here */
 unsigned char crowpuke[] = "encryptedblackbird";
 
-int main(void) {
-	
-	char key = 'C'; /* make sure this is the same key used in the encryption process */
-	printf("(+) decoded %d-bytes\n\n\r", sizeof(crowpuke)); 
-    printf("unsigned char crowpuke[] = \"");
+int main(int argc, char* argv[]) {
+
+	if (argc < 2) {
+		printf("(-) usage: xordecrypter.exe <key>");
+		return 1;
+	}
+
+	char key = argv[1][0]; /* make sure this is the same key used in the encryption process */
+
+	printf("(*) decoding with key: %c\n", key);
+	printf("(+) decoded %zd-bytes\n\n\r", sizeof(crowpuke));
+	printf("unsigned char crowpuke[] = \"");
 	for (int x = 0; x < sizeof(crowpuke) - 1; x++) {
 		crowpuke[x] = crowpuke[x] ^ key;
-        printf("\\x%02x", crowpuke[x]);
-		
+		printf("\\x%02x", crowpuke[x]);
 	}
-	
-    printf("\";");
-    return 0;
-	
+
+	printf("\";");
+	return 0;
+
 }
