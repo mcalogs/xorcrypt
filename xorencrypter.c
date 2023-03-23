62d3399d12b6cbda08d9d113e68dad435e193efd
crow <59679082+cr-0w@users.noreply.github.com>
2023-03-23 00:40:54 -0400

    Update xorencrypter.c

--- xorencrypter.c
+++ xorencrypter.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
-/* put your encrypted shellcode here */
+/* place encoded shellcode here */
 unsigned char crowpuke[] = "blackbird";
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
 
-    if (argc < 2) {
-        printf("(-) usage: xorencrypter.exe <key>");
-        return 1;
-    }
+	if (argc < 2) {
+		printf("(-) usage: xorencrypter.exe <key>");
+		return 1;
+	}
 
-    char key = argv[1][0]; /* get key from user */
+	char key = argv[1][0]; /* get key from user */
     
     printf("(*) encoding with key: %c\n", key);
-    printf("(+) encoded %d-bytes\n\n\r", sizeof(crowpuke));
+    printf("(+) encoded %zd-bytes\n\n\r", sizeof(crowpuke));
     printf("unsigned char crowpuke[] = \"");
     for (int x = 0; x < sizeof(crowpuke); x++) {
         printf("\\x%02x", crowpuke[x] ^ key);
