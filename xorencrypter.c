875ad5bc58f70048255912c4a6b7a95695f76074
crow <59679082+cr-0w@users.noreply.github.com>
2023-03-28 03:16:53 -0400

    Update xorencrypter.c

--- xorencrypter.c
+++ xorencrypter.c
@@ -13,17 +13,17 @@ int main(int argc, char* argv[]) {
 
         const char* key = argv[1]; /* get key from user */
         const size_t keyLen = strlen(key);
-        size_t shellcodeLen = strlen((const char*)crowPuke);
+        size_t shellcodeLen = sizeof(crowPuke);
 
         printf("(*) encoding with key: %s\n", key);
         printf("(+) encoded %zd-bytes\n\n\r", shellcodeLen);
         printf("unsigned char crowPuke[] = \"");
         for (int i = 0; i < shellcodeLen; ++i) {
-            crowPuke[i] ^= key[i % keyLen];
-            printf("\\x%02x", crowPuke[i]);
+                crowPuke[i] ^= key[i % keyLen];
+                printf("\\x%02x", crowPuke[i]);
         }
-
         printf("\";\n");
+        
         return 0;
 
 }
