4194c6e21b3d2f2ce4b630470f41d267fa1b2c8d
byk229 <byk229@nyu.edu>
2023-05-02 20:26:40 -0700

    feat: add file and key input
    I don't know how to use c and git

--- xordecrypter.c
+++ xordecrypter.c
@@ -1,28 +1,74 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 /* place encoded shellcode here */
-unsigned char crowPuke[] = "encryptedblackbird";
+char *crowPuke;
+int ch;
+char* key; /* get key from user */
+char* filename;
+char *read_file();
+
+char *read_file() {
+        FILE* in_file = fopen(filename, "r");
+        if (! in_file) {
+            printf("file can't be read\n");
+            exit(-1);
+         }
+
+        fseek(in_file, 0, SEEK_END);
+        int file_length = ftell(in_file);
+        fseek(in_file, 0, SEEK_SET);
+
+        char *string = malloc(sizeof(char) * (file_length+1));
+        int i = 0;
+        char c;
+
+        while ( (c = fgetc(in_file)) != EOF)
+        {
+        string[i] = c;
+        i++;
+        }
+        string[i] = '\0';
+        fclose(in_file);
+        return string;
+
+}
 
 int main(int argc, char* argv[]) {
 
-	if (argc < 2) {
-		printf("(-) usage: xordecrypter.exe <key>\n");
-		return 1;
-	}
+        while ((ch = getopt(argc, argv, "k:f:kf")) != -1)
+        {
+             switch (ch)
+             {
+             case 'k':
+                  key = optarg;
+                  break;
+              case 'f':
+                  filename = optarg;
+                  break;
+              default:
+                  (void)fprintf(stderr, "Usage: %s -k <key> -f <filename>\n", argv[0]);
+                  return(2);
+         }
+         }
+        if (key == NULL || filename == NULL) {
+                printf("(-) usage: %s -k <key> -f <filename>\n", argv[0]);
+                return 1;
+        }
+        crowPuke = read_file();
 
-	char* key = argv[1]; /* make sure this is the same key used in the encryption process */
+        printf("(*) decoding with key: %s\n", key);
+        printf("(+) decoded %zd-bytes\n\n\r", sizeof(crowPuke));
+        printf("crowPuke[] = \"");
+        size_t keyLength = strlen(key);
+        for (int x = 0; x < sizeof(crowPuke) - 1; x++) {
+                crowPuke[x] = crowPuke[x] ^ key[x % keyLength];
+                printf("\\x%02x", crowPuke[x]);
+        }
+        printf("\";\n");
 
-	printf("(*) decoding with key: %s\n", key);
-	printf("(+) decoded %zd-bytes\n\n\r", sizeof(crowPuke));
-	printf("unsigned char crowPuke[] = \"");
-	size_t keyLength = strlen(key);
-	for (int x = 0; x < sizeof(crowPuke) - 1; x++) {
-		crowPuke[x] = crowPuke[x] ^ key[x % keyLength];
-		printf("\\x%02x", crowPuke[x]);
-	}
-	printf("\";");
-	
-	return 0;
+        return 0;
 
 }
--- xorencrypter.c
+++ xorencrypter.c
@@ -1,29 +1,75 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-/* place shellcode here */
-unsigned char crowPuke[] = "blackbird";
+char *crowPuke;
+int ch;
+char* key; /* get key from user */
+char* filename;
+char *read_file();
+
+char *read_file() {
+        FILE* in_file = fopen(filename, "r");
+        if (! in_file) {
+            printf("file can't be read\n");
+            exit(-1);
+         }
+
+        fseek(in_file, 0, SEEK_END);
+        int file_length = ftell(in_file);
+        fseek(in_file, 0, SEEK_SET);
+
+        char *string = malloc(sizeof(char) * (file_length+1));
+        int i = 0;
+        char c;
+
+        while ( (c = fgetc(in_file)) != EOF)
+        {
+        string[i] = c;
+        i++;
+        }
+        string[i] = '\0';
+        fclose(in_file);
+        return string;
+
+}
 
 int main(int argc, char* argv[]) {
 
-        if (argc < 2) {
-                printf("(-) usage: xorencrypter.exe <key>\n");
+
+        while ((ch = getopt(argc, argv, "k:f:kf")) != -1)
+        {
+             switch (ch)
+             {
+             case 'k':
+                  key = optarg;
+                  break;
+              case 'f':
+                  filename = optarg;
+                  break;
+              default:
+                  (void)fprintf(stderr, "Usage: %s -k <key> -f <filename>\n", argv[0]);
+                  return(2);
+         }
+         }
+        if (key == NULL || filename == NULL) {
+                printf("(-) usage: %s -k <key> -f <filename>\n", argv[0]);
                 return 1;
         }
+        crowPuke = read_file();
 
-        const char* key = argv[1]; /* get key from user */
         const size_t keyLen = strlen(key);
         size_t shellcodeLen = sizeof(crowPuke);
 
         printf("(*) encoding with key: %s\n", key);
         printf("(+) encoded %zd-bytes\n\n\r", shellcodeLen);
-        printf("unsigned char crowPuke[] = \"");
-        for (int i = 0; i < shellcodeLen; ++i) {
+        printf("char crowPuke[] = \"");
+        for (int i = 0; i < shellcodeLen - 1; ++i) {
                 crowPuke[i] ^= key[i % keyLen];
                 printf("\\x%02x", crowPuke[i]);
         }
         printf("\";\n");
-        
+        free(crowPuke);
         return 0;
-
 }
